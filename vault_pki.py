ba9661b0fd183c4d423e64b9fc74e7fa8803573a
Daniel Wilcox <dwilcox@ripple.com>
2018-01-06 07:42:30 +0000

    Update license pre-release for lawyer cats.

--- vault_pki.py
+++ vault_pki.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
-# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#   Copyright 2018 Ripple Labs, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 
 """Salt runner module to sign certificates with the Vault PKI backend.
 
@@ -34,6 +48,7 @@ Steps that need to follow -- but the runner doesn't do:
     - run vault_pki activate $version_number
     - restart or otherwise inform servers a new certificate is in place
 """
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
